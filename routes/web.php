b36a9a8357575d49518eb25b3e4348c0e718c7ee
jelouani abdelghani <jelouaniabdelghani@gmail.com>
2024-05-30 15:42:18 +0100

    route created

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('contacts', ContactController::class);
