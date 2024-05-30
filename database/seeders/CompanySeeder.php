5143ab3f4ab7ea8690000d9d3f34f7bf8b75ba78
jelouani abdelghani <jelouaniabdelghani@gmail.com>
2024-05-30 15:43:04 +0100

    seeder created

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Company;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        Company::factory()->count(20)->create();
+    }
+}
